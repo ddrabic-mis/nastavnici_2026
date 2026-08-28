0affb7bd7426b5671bec0912b03778f728cd94b1
ddrabic-mis <info@udruga-mis.hr>
2026-08-28 12:12:01 +0200

    dan5_2

--- dan_5/baka2.py
+++ dan_5/baka2.py
@@ -1,32 +1,21 @@
-brojevi={
-  'A':3,
-  'B':3,
-  'C':3,
-  'D':4,
-  'E':4,
-  'F':4,
-  'G':5,
-  'H':5,
-  'I':5,
-  'J':6,
-  'K':6,
-  'L':6,
-  'M':7,
-  'N':7,
-  'O':7,
-  'P':8,
-  'Q':8,
-  'R':8,
-  'S':8,
-  'T':9,
-  'U':9,
-  'V':9,
-  'W':10,
-  'X':10,
-  'Y':10,
-  'Z':10,
-}
+brojevi=(
+  'ABC',
+  'DEF',
+  'GHI',
+  'JKL',
+  'MNO',
+  'PQRS',
+  'TUV',
+  'WXYZ',
+)
+
+def odredi_sekunde(slovo):
+  for i,slova in enumerate(brojevi):
+    if slovo in slova:
+      return i+3
+  return 0
+
 sekunde=0
 for znak in input():
-  sekunde+=brojevi[znak]
+  sekunde+=odredi_sekunde(znak)
 print(sekunde)
